580797f621dee675dc0e09ceb411fadadfdf0ca0
Nathalie Lysefeldt <lysefeldt@outlook.com>
2024-08-13 10:55:32 +0200

    removed component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import { useState } from "react";
 import "./App.css";
-import CountButton from "./components/CountButton";
 import TodoForm from "./components/TodoForm";
 
 function App() {
-  const [todos, setTodos] = useState<string[]>([]);
+	const [todos, setTodos] = useState<string[]>([]);
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <CountButton />
+	return (
+		<>
+			<h1>Vite + React</h1>
 
-      <TodoForm onSubmit={(text) => setTodos([...todos, text])} />
+			<TodoForm onSubmit={(text) => setTodos([...todos, text])} />
 
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo}>{todo}</li>
-        ))}
-      </ul>
-    </>
-  );
+			<ul>
+				{todos.map((todo) => (
+					<li key={todo}>{todo}</li>
+				))}
+			</ul>
+		</>
+	);
 }
 
 export default App;
